df05f49aa7bdf931d9a2caba63dc5c5f6e720fc7
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2019-11-20 17:06:06 +0100

    added processing files

---
+++ process_raw_obj.py
@@ -0,0 +1,17 @@
+import randomizePointCloud
+import pcSamplingRayShapenet
+from easydict import EasyDict
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--shapenet_path", help="Input Folder", required=True)
+	parser.add_argument('--virtualscan', type=str, help='Path to the virtual scanner executable', required=True)
+	parser.add_argument("--limit", help="Max number of points in the output cloud", default=0, type=int)
+
+	args = parser.parse_args()
+	pcSamplingRayShapenet.shoot_rays(EasyDict(args.__dict__))
+	randomizePointCloud.shuffle_folder(EasyDict(args.__dict__))
+
+
+if __name__ == "__main__":
+	main()
